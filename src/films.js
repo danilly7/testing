3e574e1644a36a69bdaadcc847fca6a3f2bdbec2
Danilly Condori <danilly7@gmail.com>
2024-10-15 12:43:02 +0200

    Exercise 1 and Exercise 2

--- src/films.js
+++ src/films.js
@@ -1,23 +1,31 @@
-// Exercise 1: Get the array of all directors.
-function getAllDirectors(array) {
-  let result =  ???;
+// ----------------------------------------------------------------------------------------------------------- Exercise 1: Get the array of all directors
+
+const movies = require("./data");
+
+function getAllDirectors(movies) {
+  let result = movies.map(movie => movie.director);
   console.log("EXERCICE 1 ->", result);
   return result;
 }
 
-// Exercise 2: Get the films of a certain director
+// ------------------------------------------------------------------------------------------------------- Exercise 2: Get the films of a certain director
+
 function getMoviesFromDirector(array, director) {
- 
+  let result = array.filter(a => a.director === director)
+  return result;
 }
 
-// Exercise 3: Calculate the average of the films of a given director.
+console.log("EXERCISE 2 director: Charles Chaplin →", getMoviesFromDirector(movies, "Charles Chaplin"))
+
+// ------------------------------------------------------------------------------------- Exercise 3: Calculate the average of the films of a given director
+
 function moviesAverageOfDirector(array, director) {
-  
+
 }
 
 // Exercise 4:  Alphabetic order by title 
 function orderAlphabetically(array) {
-  
+
 }
 
 // Exercise 5: Order by year, ascending
@@ -37,7 +45,7 @@ function hoursToMinutes() {
 
 // Exercise 8: Get the best film of a year
 function bestFilmOfYear() {
-  
+
 }
 
 
